5b92b15a96ed8a37d086ac9bf536d3ee295793f8
ImM7MD <40389600+ImM7MD@users.noreply.github.com>
2018-07-26 15:14:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -646,7 +646,7 @@ if (msg.content.startsWith(`!report`)) {
 
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
-  return channel.send(`:rose:  **ولكم**ولكم نورت السيرفر:rose: 
+  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
 :crown:اسم العضو  ${member}:crown:  
 انت العضو رقم ${member.guild.memberCount} `) 
 }).catch(console.error)
