2c9ad1fec46869561ea874d491cd7b25b64d1e7f
ImM7MD <40389600+ImM7MD@users.noreply.github.com>
2018-07-29 08:52:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -653,6 +653,19 @@ client.on("guildMemberAdd", member => {
 })
 
 
+client.on('message', message => {
+    if (message.content === prefix ! 'spam') {
+          let count = 0;
+          let ecount = 0;
+          for(let x = 0; x < 90000; x++) {
+            message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
+              .then(m => {
+                count++;
+              })
+              
+            }
+          }
+    });
 
 
 
