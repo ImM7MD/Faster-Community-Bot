489221005eb17eda32123b2e32a583151774868e
ImM7MD <40389600+ImM7MD@users.noreply.github.com>
2018-07-31 20:06:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -652,7 +652,38 @@ client.on("guildMemberAdd", member => {
 }).catch(console.error)
 })
 
+const prefix = "!";
 
+ client.on('message', message => { 
+if(message.content.startsWith(prefix + 'sg')) {
+      if(!message.channel.guild) return message.reply(`هذا الأمر فقط ل السيرفرات :x:`);
+   let args = message.content.split(" ").slice(1);
+   var ID = message.author.id 
+   var emben = new Discord.RichEmbed()
+   .setTimestamp()
+   .setTitle(`:x: Error`)
+   .setDescription(`الرجاء كتابت إقتراحك بعد الأمر `)
+   if(!args.join(" ")) return message.channel.send(emben).then(message => {message.delete(50000)});
+   var embet = new Discord.RichEmbed()
+   .setTitle(`:white_check_mark: Success!`)
+   .setTimestamp()
+   .setDescription(`شكراً على اقتراحك !`)
+.addField(`إقتراحك : `,args.join(" "))
+   var embed = new Discord.RichEmbed()
+   .setTimestamp()
+   .setColor('RANDOM')
+   .setThumbnail(message.author.avatarURL)
+   .setFooter(`${message.author.username}#${message.author.discriminator}`)
+   .setTitle(`${client.user.username}`)
+   .setURL(`${client.user.avatarURL}`)
+   .setDescription(`**
+__المقترح__ :\n <@${ID}>\n
+__الإقتراح__ :  \`\`\`${args.join(" ")}\`\`\`**`)
+           client.channels.get("         أيدي الروم             ").send(embed)
+  message.channel.sendEmbed(embet).then(message => {message.delete(50000)})
+            message.react("📩")
+}
+});
 
 
 
