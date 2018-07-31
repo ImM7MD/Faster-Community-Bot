ce7e5cfcb77e54c338a5675968a71c28993710f7
ImM7MD <40389600+ImM7MD@users.noreply.github.com>
2018-07-31 19:44:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -679,7 +679,7 @@ if(message.content.startsWith(prefix + 'sg')) {
    .setDescription(`**
 __المقترح__ :\n <@${ID}>\n
 __الإقتراح__ :  \`\`\`${args.join(" ")}\`\`\`**`)
-           client.channels.get("         473899151197732864             ").sendEmbed(embed)
+           client.channels.get("473899151197732864").sendEmbed(embed)
   message.channel.sendEmbed(embet).then(message => {message.delete(50000)})
             message.react("📩")
 }
