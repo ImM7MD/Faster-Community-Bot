e2893c530dfd875f4a506bb643bfc33a4d3a882e
ImM7MD <40389600+ImM7MD@users.noreply.github.com>
2018-08-01 12:41:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -691,7 +691,7 @@ client.on('message', message => {
       let user = message.mentions.users.first() || message.author
       let personalInvites = invs.filter(i => i.inviter.id === user.id);
       let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-message.channel.send(`${user} has ${عدد دعوات} invites.`);
+message.channel.send(`${user} has ${inviteCount} invites.`);
 });
   }
 });
