44b81dd96e78ef7f3ffadffe61fc4ff97ca3ea31
ImM7MD <40389600+ImM7MD@users.noreply.github.com>
2018-08-05 22:21:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -963,7 +963,22 @@ client.on('message', message => {
         }
     });
 
-
+client.on('voiceStateUpdate', (u, member) => {
+  var parent = '475759820742328320';
+  var channel = '475759986325061642';
+  if(member.voiceChannel === null || member.voiceChannel !== member.guild.channels.get(channel)) return console.log(`${member.user.username}'s channel isnt the needed one.`);
+  member.guild.createChannel(`${member.user.username}`, 'voice').then(c => {
+    if(!c) return;
+    c.setParent(parent);
+    member.setVoiceChannel(c);
+    setInterval(() => {
+      if(!c) return;
+      if(c.members.size === 0) {
+        c.delete();
+      }
+    }, 30000);
+  });
+});
 
 
 
