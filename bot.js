540c36895bfcacf8c67785bbdfb6bb0da4f3b777
ImM7MD <40389600+ImM7MD@users.noreply.github.com>
2018-08-28 17:07:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -986,6 +986,34 @@ client.on('voiceStateUpdate', (u, member) => {
 
 
 
+  client.on('ready',async () => {
+setInterval(function(){
+var currentTime = new Date(),
+hours = currentTime.getHours() + 3 ,
+ReBeeL = currentTime.getMinutes(),
+ReBeeeL = currentTime.getSeconds(),
+Codes = currentTime.getFullYear(),
+CodeS = currentTime.getMonth() + 1,
+CoDeS = currentTime.getDate()
+if (ReBeeL < 10) {
+ReBeeL = "0" + ReBeeL;
+}
+var suffix = "AM";
+if (hours >= 12) {
+suffix = "PM";
+hours = hours - 12;
+}
+if (hours == 0) {
+hours = 12;
+}
+client.channels.find('id', '484015956788772864').setName(`Time - ${hours} : ${ReBeeL} : ${ReBeeeL} ${suffix}`) 
+client.channels.find('id', '484015985075027968').setName(`Date : ${Codes} - ${CodeS} - ${CoDeS}`)
+}, 1000);
+});
+
+
+
+
 
 
 
